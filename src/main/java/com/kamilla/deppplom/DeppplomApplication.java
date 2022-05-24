203e78af5390d63f4c5183d2f593b7b6e9fb388e
kamillagalliamutdinova <s_kamill@mail.ru>
2022-05-24 18:50:16 +0500

    new: initial commit

---
+++ src/main/java/com/kamilla/deppplom/DeppplomApplication.java
@@ -0,0 +1,13 @@
+package com.kamilla.deppplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeppplomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeppplomApplication.class, args);
+    }
+
+}
