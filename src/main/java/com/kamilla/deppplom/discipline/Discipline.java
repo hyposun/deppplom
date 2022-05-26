888d302c3d6a3a55dc766a684401da5647dcdc3c
shockoway <nasyrov_r@ufanet.ru>
2022-05-26 14:28:45 +0500

    new: работа с дисциплинами !wip

---
+++ src/main/java/com/kamilla/deppplom/discipline/Discipline.java
@@ -0,0 +1,21 @@
+package com.kamilla.deppplom.discipline;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Table
+@Entity
+@Data
+public class Discipline {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String title;
+
+}
