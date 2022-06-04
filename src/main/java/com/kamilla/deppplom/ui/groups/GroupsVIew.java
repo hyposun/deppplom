f0bf8431ee623c3d91d1486861e48c0a017f6a65
shockoway <nasyrov_r@ufanet.ru>
2022-06-04 20:26:42 +0500

    new: ui: загрузка групп из файла

--- src/main/java/com/kamilla/deppplom/ui/groups/GroupsVIew.java
+++ src/main/java/com/kamilla/deppplom/ui/groups/GroupsVIew.java
@@ -2,34 +2,46 @@ package com.kamilla.deppplom.ui.groups;
 
 import com.kamilla.deppplom.groups.StudentGroup;
 import com.kamilla.deppplom.groups.StudentGroupRepository;
+import com.kamilla.deppplom.groups.data.StudentGroupDataService;
 import com.kamilla.deppplom.ui.BaseLayout;
-import com.kamilla.deppplom.users.User;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import javax.annotation.security.RolesAllowed;
+import java.util.List;
+
+import static com.kamilla.deppplom.ui.utils.UIUtils.errorNotification;
+import static com.kamilla.deppplom.ui.utils.UIUtils.successNotification;
 
 @Route(value = "groups", layout = BaseLayout.class)
 @RolesAllowed("ADMIN")
 @PageTitle("Deppplom | Группы")
 public class GroupsVIew extends VerticalLayout {
 
+    private StudentGroupDataService studentGroupDataService;
     private StudentGroupRepository repository;
     private GroupEditor editor;
+
+    private MemoryBuffer memoryBuffer = new MemoryBuffer();
+    private Upload upload = new Upload(memoryBuffer);
 
     private Grid<StudentGroup> grid = new Grid<>(StudentGroup.class);
     private TextField filter = new TextField("", "Название");
     private Button addNew = new Button("Добавить", VaadinIcon.PLUS.create());
-    private HorizontalLayout toolbar = new HorizontalLayout(filter, addNew);
+    private HorizontalLayout toolbar = new HorizontalLayout(filter, addNew, upload);
 
-    public GroupsVIew(StudentGroupRepository repository, GroupEditor editor) {
+
+    public GroupsVIew(StudentGroupDataService studentGroupDataService, StudentGroupRepository repository, GroupEditor editor) {
+        this.studentGroupDataService = studentGroupDataService;
         this.repository = repository;
         this.editor = editor;
         add(toolbar, grid);
@@ -38,6 +50,12 @@ public class GroupsVIew extends VerticalLayout {
     }
 
     private void setupInteractivity() {
+
+        upload.setUploadButton(new Button("Импорт", VaadinIcon.FILE.create()));
+        upload.setAcceptedFileTypes(".csv");
+        upload.setMaxFiles(1);
+        upload.setDropAllowed(false);
+        upload.addSucceededListener(event -> importFromFile());
 
         filter.setValueChangeMode(ValueChangeMode.EAGER);
         filter.addValueChangeListener(event -> showGroups(event.getValue()));
@@ -48,10 +66,20 @@ public class GroupsVIew extends VerticalLayout {
 
         editor.setOnClose(() -> showGroups(filter.getValue()));
 
-        addNew.addClickListener(event -> {
-            editor.editGroup(new StudentGroup());
-        });
+        addNew.addClickListener(event -> editor.editGroup(new StudentGroup()));
 
+    }
+
+    private void importFromFile() {
+        try {
+            List<StudentGroup> users = studentGroupDataService.getGroupsFromCsv(memoryBuffer.getInputStream());
+            upload.clearFileList();
+            showGroups("");
+            successNotification("Групп успешно загружено: " + users.size(), 2);
+        } catch (Exception e) {
+            upload.clearFileList();
+            errorNotification("Не удалось загрузить группы из файла, проверьте корректность данных: " + e.getMessage(), 3);
+        }
     }
 
     private void showGroups(String name) {
