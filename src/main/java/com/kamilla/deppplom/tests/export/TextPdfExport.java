9ef11337703272cd8425b6b3f3722c1fae6f0de7
kamillagalliamutdinova <s_kamill@mail.ru>
2022-06-13 22:06:56 +0500

    new: выгрузка вариантов в pdf формате !wip

---
+++ src/main/java/com/kamilla/deppplom/tests/export/TextPdfExport.java
@@ -0,0 +1,102 @@
+package com.kamilla.deppplom.tests.export;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.kamilla.deppplom.question.impl.closedquestion.ClosedQuestion;
+import com.kamilla.deppplom.question.impl.openquestion.OpenQuestion;
+import com.kamilla.deppplom.question.impl.orderedclosedquestion.OrderedClosedQuestion;
+import com.kamilla.deppplom.question.model.Question;
+import com.kamilla.deppplom.tests.model.TestVersion;
+
+import java.io.FileOutputStream;
+
+public class TextPdfExport {
+
+    private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD);
+    private static Font normalFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.BLACK);
+
+    private void getPdfDocument(String path, TestVersion version) {
+        try {
+            Document document = new Document();
+            PdfWriter.getInstance(document, new FileOutputStream(path));
+            document.open();
+            addPage(document,version);
+            document.close();
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+
+    private void addPage(Document document,TestVersion version) throws DocumentException {
+
+        addContent(version,document);
+        document.newPage();
+    }
+
+    private void addEmptyLine(Paragraph paragraph, int number) {
+        for (int i = 0; i < number; i++) {
+            paragraph.add(new Paragraph(" "));
+        }
+    }
+
+    private void addContent(TestVersion version,Document document) throws DocumentException {
+
+        Paragraph paragraph = new Paragraph();
+        paragraph.add(new Paragraph("Экзаменационный билет: ", subFont));
+        addEmptyLine(paragraph,5);
+        document.add(paragraph);
+
+        var questionList = version.getQuestions();
+        int i=1;
+        for (Question question : questionList) {
+            Paragraph questionPar = new Paragraph(i + "." + " " + question.getTitle(), normalFont);
+            getQuestionTypeContent(question,document);
+            document.add(questionPar);
+            i++;
+        }
+    }
+
+    private void getQuestionTypeContent(Question question, Document document) throws DocumentException {
+        var questionType = question.getType();
+        switch (questionType) {
+            case OPENED: {
+                var openQuestion = (OpenQuestion)question;
+                Phrase phrase = new Phrase("\n");
+                document.add(phrase);
+            }
+            case CLOSED: {
+                var closedQuestion = (ClosedQuestion)question;
+                getClosedQuestionAnswer(closedQuestion, document);
+            }
+            case CLOSED_ORDERED: {
+                var orderedClosedQuestion = (OrderedClosedQuestion)question;
+                getOrderedClosedQuestionAnswer(orderedClosedQuestion, document);
+            }
+        }
+    }
+
+    private void getClosedQuestionAnswer(ClosedQuestion question, Document document) throws DocumentException {
+        var answers = question.getOptions();
+        int i = 1;
+        for (ClosedQuestion.Option answer : answers) {
+            Paragraph answerTitle = new Paragraph(i + "." + " " + answer.getTitle(),normalFont);
+            answerTitle.setIndentationLeft(50);
+            document.add(answerTitle);
+            i++;
+        }
+    }
+
+    private void getOrderedClosedQuestionAnswer(OrderedClosedQuestion question, Document document) throws DocumentException {
+        var answers = question.getOptions();
+        int i = 1;
+        for (OrderedClosedQuestion.Option answer : answers) {
+            Paragraph answerTitle = new Paragraph( i+ "." + " " + answer.getTitle(),normalFont);
+            answerTitle.setIndentationLeft(50);
+            document.add(answerTitle);
+            i++;
+        }
+    }
+
+}
