2b96aa9ca3b13fbfa8e9959d08cebc331fe6c636
shockoway <nasyrov_r@ufanet.ru>
2022-06-27 15:39:45 +0500

    chg: смена алгоритма шифрования пароля

--- src/main/java/com/kamilla/deppplom/security/VaadinWebSecurityConfig.java
+++ src/main/java/com/kamilla/deppplom/security/VaadinWebSecurityConfig.java
@@ -2,21 +2,25 @@ package com.kamilla.deppplom.security;
 
 import com.kamilla.deppplom.ui.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class VaadinWebSecurityConfig extends VaadinWebSecurityConfigurerAdapter {
 
+    @Value("${service.auth.secret}")
+    private String secret;
+
     @Bean
     public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+        return new Pbkdf2PasswordEncoder(secret);
     }
 
     @Override
